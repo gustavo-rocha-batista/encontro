87efc943fa2f371c6d4380bec6777a51d880d41c
gustavo-rocha-batista <gustavobatista0129@gmail.com>
2026-09-12 16:29:20 -0300

    Adicionando botõtes e data

---
+++ script.js
@@ -0,0 +1,10 @@
+const header = document.querySelector('header');
+const headerWidth = header.offsetWidth;
+const headerHeight = header.offsetHeight;
+const bodyHeight = document.querySelector('body').offsetHeight;
+
+function changePos() {
+    const nao = document.getElementById('no');
+    
+    
+}
