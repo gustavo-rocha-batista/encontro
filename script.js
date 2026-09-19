611ce82bfb8843a51a6a5e94aecb28aba9ac1a85
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-18 22:55:35 -0300

    Calendly Implementado

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const header = document.querySelector('header')
 const body = document.querySelector('body')
 const sim = document.getElementById('yes');
 const nao = document.getElementById('no');
-const marcar = document.getElementById('schedule');
-const calendario = document.getElementById('calendario');
+const calendly = document.querySelector('.calendly-inline-widget');
 
-const headerWidth = header.offsetWidth;
 const headerHeight = header.offsetHeight;
 const bodyHeight = body.offsetHeight;
 const bodyWidth = body.offsetWidth;
+
 nao.style.position = 'relative';
-calendario
+calendly.style.display = 'none';
+
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -21,20 +21,33 @@ function checkCollision(x, y, element, other) {
     return x < other.offsetLeft + other.offsetWidth
         && x + element.offsetWidth > other.offsetLeft
         && y < other.offsetTop + other.offsetHeight
-        && y + element.offsetHeight > other.offsetTop;  
+        && y + element.offsetHeight > other.offsetTop;
 }
 
 function changePos() {
     let newX, newY;
     do {
-        newY = getRandomInt(0-headerHeight, bodyHeight);
-        newX = getRandomInt(0-headerWidth, headerWidth);        
-    } while (checkCollision(newX, newY, nao, sim) || checkCollision(newX, newY, nao, marcar) 
-        || checkCollision(newX, newY, nao, calendario))
+        newY = getRandomInt(0 - (headerHeight / 2), bodyHeight - headerHeight - nao.offsetHeight - 190);
+        newX = getRandomInt(0 - (bodyWidth / 2) + nao.offsetWidth, bodyWidth / 2 - nao.offsetWidth);
+    } while (checkCollision(newX, newY, nao, sim));
     nao.style.top = newY + 'px';
     nao.style.left = newX + 'px';
 }
 
-sim.addEventListener('click', () =>{
+sim.addEventListener('click', () => {
     nao.style.display = "none";
+    sim.style.display = "none";
+    calendly.style.display = "block";
 });
+
+const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+if (isMobile) {
+    nao.addEventListener('click', () => {
+        changePos();
+    });
+} else {
+    nao.addEventListener('mouseover', () => {
+        changePos();
+    });
+}
